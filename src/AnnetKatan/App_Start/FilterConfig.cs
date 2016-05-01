38b800c27a2daf3cf7ea3d915c829c817e997e3b
Oleg Burov <oleg.burov@outlook.com>
2016-05-01 13:06:27 -0700

    Added server side telemetry for exceptions.

--- src/AnnetKatan/App_Start/FilterConfig.cs
+++ src/AnnetKatan/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace AnnetKatan
     public static void RegisterGlobalFilters(GlobalFilterCollection filters)
     {
       filters.Add(new HandleErrorAttribute());
+      filters.Add(new AiHandleErrorAttribute());
     }
   }
 }
