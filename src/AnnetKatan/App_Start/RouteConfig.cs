cb0fabb3b8cf80d0a49b04ba442c47434a7f75b1
Oleg Burov <oleg.burov@outlook.com>
2016-07-15 15:15:13 -0700

    Added a new controller to handle a direct reference to Portfolio.pdf.

--- src/AnnetKatan/App_Start/RouteConfig.cs
+++ src/AnnetKatan/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace AnnetKatan
           url: "{controller}/{action}/{id}",
           defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
       );
+
+      routes.MapRoute(
+          name: "Portfolio",
+          url: "{controller}/{action}",
+          defaults: new { controller = "Portfolio", action = "Index" }
+      );
     }
   }
 }
---
+++ src/AnnetKatan/Controllers/PortfolioController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnnetKatan.Controllers
+{
+  public class PortfolioController : Controller
+  {
+    private const string PortfolioPdfUrl = "http://media.annetkatan.com/files/Portfolio.pdf";
+
+    // GET: Portfolio
+    public ActionResult Index()
+    {
+      return Redirect(PortfolioPdfUrl);
+    }
+  }
+}
