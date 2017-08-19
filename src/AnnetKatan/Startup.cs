34f0640829b1f3b39cd706607b8ca24dd009f042
Oleg Burov <oleg.burov@outlook.com>
2017-08-19 16:43:36 -0700

    Remove AddJsonFile, AddUserSecrets and AddEnvironmentVariables.
    Remove AddJsonFile, AddUserSecrets and AddEnvironmentVariables, because
    because the
    'CreateDefaultBuilder' method already adds both of them in
    ASP.NET Core 2.0.

--- src/AnnetKatan/Startup.cs
+++ src/AnnetKatan/Startup.cs
@@ -8,24 +8,12 @@ namespace AnnetKatan
 {
   public class Startup
   {
-    public Startup(IHostingEnvironment env)
+    public Startup(IConfiguration configuration)
     {
-      var builder = new ConfigurationBuilder()
-          .SetBasePath(env.ContentRootPath)
-          .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
-
-      if (env.IsDevelopment())
-      {
-        // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
-        builder.AddUserSecrets("1d73293a-0a5d-4111-a8f4-74d7edfc8f9c");
-      }
-
-      builder.AddEnvironmentVariables();
-      Configuration = builder.Build();
+      Configuration = configuration;
     }
 
-    public IConfigurationRoot Configuration { get; }
+    public IConfiguration Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
